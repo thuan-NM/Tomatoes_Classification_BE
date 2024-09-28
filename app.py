17bceddbbb5eba161b40dead825a7d23f447a10c
ThuanNguyenITVN <nguyenminhthuan2003st@gmail.com>
2024-09-28 16:31:56 +0700

    Update for deploy

--- app.py
+++ app.py
@@ -45,4 +45,4 @@ def predict():
     return jsonify({'error': 'Invalid request'}), 400
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=int(os.environ.get('PORT', 5000)))
+    app.run(debug=True)
